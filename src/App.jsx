468620cfc20586f5c816e1f7208024cc158400c8
Aditya <sherkiaditya@gmail.com>
2023-09-30 18:09:38 +0530

    Login-Form-With-Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import { useState } from "react";
 // import AdvancedForm from "./components/AdvancedForm";
 import Register from "./components/Register";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
-  const [view, setView] = useState("basic");
+  const [view, setView] = useState("login");
   return (
     <div className="App">
       <nav>
         <h3
-          onClick={() => setView("basic")}
-          style={{ color: view === "basic" ? "#fff" : "" }}
+          onClick={() => setView("register")}
+          style={{ color: view === "register" ? "#fff" : "" }}
         >
           Register
         </h3>
         <h3
-          onClick={() => setView("advanced")}
-          style={{ color: view === "advanced" ? "#fff" : "" }}
+          onClick={() => setView("login")}
+          style={{ color: view === "login" ? "#fff" : "" }}
         >
           Log In
         </h3>
       </nav>
-      {view === "basic" ? <Register /> : <AdvancedForm />}
+      {view === "login" ? <Login /> : <Register />}
     </div>
   );
 }
--- src/schemas/validationSchema.js
+++ src/schemas/validationSchema.js
@@ -18,16 +18,14 @@ export const registerSchema = yup.object().shape({
     .required("Required"),
 });
 
-// export const advancedSchema = yup.object().shape({
-//   username: yup
-//     .string()
-//     .min(3, "Username must be at least 3 characters long")
-//     .required("Required"),
-//   jobType: yup
-//     .string()
-//     .oneOf(["designer", "developer", "manager", "other"], "Invalid Job Type")
-//     .required("Required"),
-//   acceptedTos: yup
-//     .boolean()
-//     .oneOf([true], "Please accept the terms of service"),
-// });
+export const loginSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .required("Required"),
+  password: yup
+    .string()
+    .min(5)
+    .matches(passwordRules, { message: "Please create a stronger password" })
+    .required("Required"),
+});
