2e1634354434a783e2e5e1688b5848a3be87267c
Aditya <sherkiaditya@gmail.com>
2023-09-30 17:38:06 +0530

    Registration-Form-With-Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState } from "react";
+// import AdvancedForm from "./components/AdvancedForm";
+import Register from "./components/Register";
+import "./App.css";
 
-export default function App() {
+function App() {
+  const [view, setView] = useState("basic");
   return (
-    <div>Hello World!!</div>
-  )
+    <div className="App">
+      <nav>
+        <h3
+          onClick={() => setView("basic")}
+          style={{ color: view === "basic" ? "#fff" : "" }}
+        >
+          Register
+        </h3>
+        <h3
+          onClick={() => setView("advanced")}
+          style={{ color: view === "advanced" ? "#fff" : "" }}
+        >
+          Log In
+        </h3>
+      </nav>
+      {view === "basic" ? <Register /> : <AdvancedForm />}
+    </div>
+  );
 }
 
+export default App;
